1b368b433b705af0ed972e6c982af352ac68aa64
Jess <demonswaltz@gmail.com>
2017-11-18 09:01:51 -0600

    initial commit

---
+++ JustJava/app/src/main/java/com/demonswaltz/justjava/MainActivity.java
@@ -0,0 +1,22 @@
+package com.demonswaltz.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private int quantity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void submitOrder(View view);{
+
+    }
+
+    private int calculatePrice(){
+
+    }
+    public int increment(quantity);{}
+    public int decrement(quantity){}
+}
